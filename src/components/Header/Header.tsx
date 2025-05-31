b876e7c5e7743f820ddb09b5d0e998adc1720d64
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-05-31 19:56:03 -0300

    update commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss'
 export function Header() {
   return (
     <header className={styles.header}>
-      <div><span>Ignite Lab</span> | <h2>REACTJS</h2></div>
+      <div><span>Event Platform</span></div>
     </header>
   )
 }
